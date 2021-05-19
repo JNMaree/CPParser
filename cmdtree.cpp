930e82c6d7969c0b90fa5c70b042bfa1fe250ca3
Johann Maree <jnmaree@protonmail.com>
2021-05-19 21:56:11 +0200

    namespace update

--- cmdtree.cpp
+++
@@ -1,7 +0,0 @@
-#include "cmdtree.hpp"
-
-namespace cpparse
-{
-    std::vector<std::unique_ptr<cmd>> cmdVector;
-
-}//namespace:command
--- cmdtree.hpp
+++
@@ -1,9 +0,0 @@
-#include <vector>
-#include <memory>
-#include "cmd.hpp"
-
-namespace cpparse
-{
-    extern std::vector<std::unique_ptr<cmd>> cmdVector;
-
-}//namespace:command
